c423cb0e49b4316feb1beeed3536682c8346845d
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-08-27 23:29:16 +0100

    Add dynamic aspect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './index.css';
+
+const dynamicList = document.querySelector('.score-conatiner');
+const listItems = document.createElement('li');
+const playerName = document.createElement('span');
+playerName.textContent = 'Name';
+const playerScore = document.createElement('span');
+playerScore.textContent = '100';
+const seperator = document.createElement('span');
+seperator.textContent = ': ';
+listItems.appendChild(playerName);
+listItems.appendChild(seperator);
+listItems.appendChild(playerScore);
+dynamicList.appendChild(listItems);
